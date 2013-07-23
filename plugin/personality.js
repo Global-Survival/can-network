ae2e966473594b8efe1b8faee88c01cfc6e84515
SeH <seh999@gmail.com>
2013-07-22 20:13:54 -0400

    personality.js added

---
+++ plugin/personality.js
@@ -0,0 +1,11 @@
+/*
+http://en.wikipedia.org/wiki/Myers-Briggs_Type_Indicator
+http://en.wikipedia.org/wiki/Enneagram_of_Personality
+
+http://slackprop.wordpress.com/2012/12/07/machine-learning-and-personality-type/
+http://www.typealyzer.com/
+
+CBS Surveys
+https://docs.google.com/spreadsheet/ccc?key=0AifmjEDwh_fedHZuVm1oLTRrdUZwX2Vrb2dOZlVlbkE&usp=sharing
+
+*/
