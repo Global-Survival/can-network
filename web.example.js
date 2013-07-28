13038aed7bf70950a7076084a70382ae42ff6aca
SeH <seh999@gmail.com>
2013-07-28 14:41:54 -0400

    updates

--- web.example.js
+++ web.example.js
@@ -8,17 +8,17 @@ require('./server/web.js').start(
     8080, 
     'localhost/netention_db_0',
         
-    function(server) {	
+    function(netention) {	
     
-        server.configFile = 'client.js';
-        server.permissions['authenticate_to_configure_plugins'] = false;
-        server.permissions['authenticate_to_create_objects'] = false;
-        server.permissions['authenticate_to_delete_objects'] = false;
-        server.permissions['authenticate_to_proxy_http'] = false;
-        server.permissions['authenticate_to_create_profiles'] = false;
-        server.permissions['anyone_to_enable_or_disable_plugin'] = true;
+        netention.configFile = 'client.js';
+        netention.permissions['authenticate_to_configure_plugins'] = false;
+        netention.permissions['authenticate_to_create_objects'] = false;
+        netention.permissions['authenticate_to_delete_objects'] = false;
+        netention.permissions['authenticate_to_proxy_http'] = false;
+        netention.permissions['authenticate_to_create_profiles'] = false;
+        netention.permissions['anyone_to_enable_or_disable_plugin'] = true;
 
-        //server.permissions['twitter_key'] = 'CONSUMER_KEY:CONSUMER_SECRET';
+        //netention.permissions['twitter_key'] = 'CONSUMER_KEY:CONSUMER_SECRET';
 
 		//Plugins to auto-enable
 		netention.enablePlugins =  [ /* 'earthquake.js', 'rss.js' */ ];
