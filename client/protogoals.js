0a21bbe5dbdbde70ec1ef4580441ed67a5f78c03
SeH <seh999@gmail.com>
2013-09-02 03:08:42 -0400

    protogoals support

---
+++ client/protogoals.js
@@ -0,0 +1,106 @@
+/* 
+
+Acquire Item (ex: store purchase or borrow/trade)
+
+Consume Item
+	Quantity Range (Dose)
+	Frequency
+	Ingestion Method
+		Orally
+		Sublingually
+		IV
+	…
+	Contraindications/Indications
+
+Gather / Ready / Deploy / Attend_to Item (get it ready for use)
+	saving minutes in the case of an emergency could save a life
+
+Share Item
+
+Travel
+
+Contact
+
+Relax / Do Nothing / Be Patient / Breathe
+
+
+Personal Conditions
+	Biometrics
+		Age
+		Weight
+		Height
+		Gender
+		Temperature
+		Ethnicity
+		Blood type
+		Genetics
+		Pulse Rate & Variability
+		Blood Pressure
+		Breathing Rate
+		...
+	Hunger / Thirst
+		...
+	Memory
+	Focus
+	Mood
+		Stability
+		Presence of Depression
+		Presence of Euphoria
+	Energy
+	Relax
+	Athletic Performance
+	Illness
+	Injury
+		[per body part]
+	Need to Detox and Stop...
+		Alcohol Addiction
+		Opiate Addiction
+		Cocaine / Amphetamine Addiction
+		Benzodiazepine Addiction
+		Nicotine Addiction
+		Sugar Addiction
+		Other Addictions and their cravings
+
+Environment Conditions
+	Time 
+		of Day
+		of Year
+	Weather
+		Rain Storm
+		Snow
+		Tropical Storm
+		Hurricane
+		Tornado
+		Tsunami
+		..
+	Pollution
+		Air/Water Quality
+		Radiation
+		Electrosmog
+		...
+	Crime
+	Economic
+	Sociopolitical
+		Vote
+		Taxes
+		...
+	Cultural
+		Holidays
+	Farming / Plant Maintenance
+
+Resource Inventory
+	[Item, Quantity]
+	Cash [Currency, Quantity]
+
+Social Conditions
+	Relationship Quality
+		Animals / Pets
+		Strangers
+		Coworker
+		Student / Teacher
+		Friend
+		Family
+		Roommate
+		Boyfriend/Girlfriend
+		Spouse
+*/
